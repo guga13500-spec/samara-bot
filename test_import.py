0b800648fe2d3334e3fe1315ee5fa7c1b2018326
guga13500-spec <guga13500@gmail.com>
2026-07-23 18:37:32 -0300

    Jarvis - test import script

---
+++ test_import.py
@@ -0,0 +1,25 @@
+import os, sys, ctypes
+
+sdk_path = os.environ.get('SDK_PATH', '')
+if not sdk_path:
+    print("SDK_PATH not set!")
+    sys.exit(1)
+
+lib = os.path.join(sdk_path, 'libTeamTalk5.so')
+print("lib path:", lib)
+print("lib exists:", os.path.exists(lib))
+
+if not os.path.exists(lib):
+    # debug: list files
+    tt_dir = os.path.dirname(os.path.dirname(sdk_path))
+    for root, dirs, files in os.walk(tt_dir):
+        for f in files:
+            if f.endswith('.so'):
+                print("  found:", os.path.join(root, f))
+    sys.exit(1)
+
+os.environ['LD_LIBRARY_PATH'] = sdk_path
+ctypes.cdll.LoadLibrary(lib)
+from teamtalk import TeamTalkBot
+from teamtalk.enums import TeamTalkServerInfo
+print("OK!")
